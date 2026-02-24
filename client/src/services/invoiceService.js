dcf2f6aa3863b92307a4d70b690e2bb4d5fa16aa
mokshlab <mokshlab@user.noreply.com>
2026-02-24 15:42:48 +0530

    add frontend invoice API service

---
+++ client/src/services/invoiceService.js
@@ -0,0 +1,10 @@
+import api from './api';
+
+export const invoiceService = {
+  getAll: (params) => api.get('/invoices', { params }),
+  getById: (id) => api.get(`/invoices/${id}`),
+  create: (data) => api.post('/invoices', data),
+  update: (id, data) => api.put(`/invoices/${id}`, data),
+  delete: (id) => api.delete(`/invoices/${id}`),
+  getStats: () => api.get('/invoices/stats'),
+};
