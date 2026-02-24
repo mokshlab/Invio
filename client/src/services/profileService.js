d89bee640b9c0e66c727cbe0b91e33b3867f1e94
mokshlab <mokshlab@user.noreply.com>
2026-02-24 15:42:56 +0530

    add frontend profile API service

---
+++ client/src/services/profileService.js
@@ -0,0 +1,7 @@
+import api from './api';
+
+export const profileService = {
+  get: () => api.get('/profile'),
+  update: (data) => api.put('/profile', data),
+  changePassword: (data) => api.put('/profile/password', data),
+};
