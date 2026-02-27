98c1f69b970e3c8dba16a070fc5561221ca065c1
mokshlab <mokshlab@user.noreply.com>
2026-02-27 10:16:54 +0530

    add sendEmail and getEmailStatus client service methods

--- client/src/services/invoiceService.js
+++ client/src/services/invoiceService.js
@@ -7,4 +7,6 @@ export const invoiceService = {
   update: (id, data) => api.put(`/invoices/${id}`, data),
   delete: (id) => api.delete(`/invoices/${id}`),
   getStats: () => api.get('/invoices/stats'),
+  sendEmail: (id) => api.post(`/invoices/${id}/send`),
+  getEmailStatus: () => api.get('/invoices/email-status'),
 };
